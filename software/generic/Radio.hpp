fc630f5f80f4291c3a8c8fbede267aba8b4e7d35
Adam Demuri <adam.demuri@gmail.com>
2018-01-17 17:39:59 -0700

    Define a RadioPacket and the Radio interface
    Change-Id: Idc92b0b32f3b8667501956470cb8f55b0e052933

--- software/generic/Radio.hpp
+++ software/generic/Radio.hpp
@@ -1,7 +1,54 @@
 #ifndef __RADIO_H__
 #define __RADIO_H__
 
+// Arduino provides the types of the form 'int8_t'. In vanilla C++, we need to
+// include these manually.
+#ifndef ARDUINO
+#include <cstdint>
+#endif
+
+enum PacketType {
+  PING,
+};
+
+struct RadioPacket {
+  /**
+   * This is used for "mesh" networking. Upon receiving a packet, each node
+   * rebroadcasts it if it has not recently rebroadcasted a packet with that
+   * packetId.
+   */
+  uint16_t packetId;
+
+  /**
+   * The type of packet. This determines how the data is processed.
+   */
+  PacketType type;
+
+  /**
+   * The length of the data field.
+   */
+  uint8_t dataLength;
+
+  /**
+   * The raw application-layer data. The length is the max packet length (61)
+   * minus the size of the packet ID and type (3 bytes).
+   */
+  uint8_t data[58];
+};
+
 /** TODO: create an interface for the radio. */
-class Radio {};
+class Radio {
+  public:
+    /**
+     * If a packet is avilable, reads it into the provided struct and return
+     * true. If no packet is available, returns false.
+     */
+    virtual bool readPacket(RadioPacket &packet) = 0;
+
+    /**
+     * Sends the packet.
+     */
+    virtual void sendPacket(RadioPacket &packet) = 0;
+};
 
 #endif
