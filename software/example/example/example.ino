4537856d387455877ae55cdd02a3facfc638e986
Adam Demuri <adam.demuri@gmail.com>
2019-01-27 21:50:23 -0700

    Use the network time to blink the leds
    Change-Id: I8beed6aca341c61340429b57e7501597f5d25afe

--- software/example/example/example.ino
+++ software/example/example/example.ino
@@ -1,29 +1,55 @@
-#include <NetworkManager.hpp>
-#include <RFM69Radio.hpp>
 #include <FastLED.h>
+#include <NetworkManager.hpp>
+#include <RadioHeadRadio.hpp>
+#include <RadioStateMachine.hpp>
 
 const int kLedPin = 0;
-#define kNumLeds 1
+#define kNumLeds 4
 #define WS2812_PIN 6
 
 CRGB leds[kNumLeds];
 
-RFM69Radio* radio;
+RadioHeadRadio* radio;
 NetworkManager* nm;
+RadioStateMachine* stateMachine;
 
 void setup() {
-  radio = new RFM69Radio();
-  nm = new NetworkManager(radio);
+  Serial.begin(115200);
+  // Delay makes it easier to reset the board in case of failure
+  delay(2000);
 
   FastLED.addLeds<NEOPIXEL, WS2812_PIN>(leds, kNumLeds);
   pinMode(kLedPin, OUTPUT);
+
+  // Yellow LED on boot indicates a problem initializing the radio
+  FastLED.showColor(CRGB(16, 16, 0));
+  radio = new RadioHeadRadio();
+  nm = new NetworkManager(radio);
+  stateMachine = new RadioStateMachine(nm);
+
+  FastLED.showColor(CRGB(0, 0, 0));
 }
 
 const int kBufLen = 10;
 char buf[kBufLen];
 
+unsigned long printAliveAt = 0;
+
 void loop() {
-  leds[0] = CHSV(random(100), 100, 100);
-  FastLED.show();
-  delay(500);
+  stateMachine->Tick();
+
+  if (stateMachine->GetNetworkMillis() % 1000 < 300) {
+    FastLED.showColor(CRGB(32, 0, 0));
+  } else {
+    if (stateMachine->GetCurrentState() == RadioState::Master) {
+      FastLED.showColor(CRGB(0, 8, 0));
+    } else {
+      FastLED.showColor(CRGB(0, 0, 8));
+    }
+  }
+
+  if (millis() > printAliveAt) {
+    Serial.println(stateMachine->GetNetworkMillis());
+    printAliveAt = millis() + 1000;
+  }
 }
--- software/format.sh
+++ software/format.sh
@@ -2,3 +2,4 @@
 
 # Formats all of the source files using clang-format.
 clang-format -i -style=Google $(find . -name *.[ch]pp)
+clang-format -i -style=Google $(find . -name *.ino)
