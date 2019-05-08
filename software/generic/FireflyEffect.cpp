fc0f29fe1394910e32e7ca19e287398a1deb74f9
Adam Demuri <adam.demuri@gmail.com>
2019-05-07 21:01:37 -0600

    Add a Firefly effect
    This blinks the light, smoothly transitioning between in- and
    out-of-phase.
    
    Change-Id: Ie0b96a2099f1bf15b8d120e7825a1859c89c6ea2

---
+++ software/generic/FireflyEffect.cpp
@@ -0,0 +1,45 @@
+#include "FireflyEffect.hpp"
+#include "Debug.hpp"
+
+FireflyEffect::FireflyEffect(uint8_t numLeds, CRGB &color) : Effect(numLeds) {
+#ifdef ARDUINO
+  randomSeed((analogRead(A0) << 10) | analogRead(A0));
+#endif
+  offset = random(0, kBlinkPeriod / 2);
+}
+
+CRGB FireflyEffect::GetRGB(uint8_t ledIndex, uint32_t timeMs,
+                           RadioPacket *setEffectPacket) {
+  const int8_t phase = (timeMs / kPeriodMs) % 3;
+
+  uint32_t adjustedTime = 0;
+  const uint32_t periodStart = (timeMs / kPeriodMs) * kPeriodMs;
+  const uint32_t elapsedInPeriod = timeMs - periodStart;
+  const uint32_t remainingInPeriod = kPeriodMs - elapsedInPeriod;
+  if (phase == 0) {
+    // Out of sync -> in sync
+    if (offset < kBlinkPeriod / 4) {
+      adjustedTime = timeMs - (offset * remainingInPeriod) / kPeriodMs;
+    } else {
+      adjustedTime = timeMs + (offset * remainingInPeriod) / kPeriodMs;
+    }
+  } else if (phase == 1) {
+    // In sync
+    adjustedTime = timeMs;
+  } else {
+    // In sync -> out of sync
+    if (offset < kBlinkPeriod / 4) {
+      adjustedTime = timeMs - (offset * elapsedInPeriod) / kPeriodMs;
+    } else {
+      adjustedTime = timeMs + (offset * elapsedInPeriod) / kPeriodMs;
+    }
+  }
+
+  int16_t curve = sin16(adjustedTime * kSinMultiplier);
+  if (curve < 0) {
+    curve = 0;
+  }
+  return ColorFromPalette(
+      palettes[setEffectPacket->readPaletteIndexFromSetEffect()],
+      /* index */ timeMs / kBlinkPeriod, /* brightness */ curve / 256);
+}
