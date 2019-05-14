942e9de9889a040820161d3afa67a0559e76714a
Adam Demuri <adam.demuri@gmail.com>
2019-05-14 15:30:52 -0600

    Update RainbowEffect to work with single-color palettes.
    This makes it pulse the single color.
    
    Change-Id: Ic2cf0ae1bbd9aa8acd2046ed06ed0db24b3bf514

--- software/generic/RainbowEffect.cpp
+++ software/generic/RainbowEffect.cpp
@@ -5,13 +5,27 @@ RainbowEffect::RainbowEffect(uint8_t numLeds, CRGB &color)
 
 CRGB RainbowEffect::GetRGB(uint8_t ledIndex, uint32_t timeMs,
                            RadioPacket *setEffectPacket) {
-  if (numLeds < 8) {
-    return ColorFromPalette(
-        palettes[setEffectPacket->readPaletteIndexFromSetEffect()], timeMs / 16,
-        128);
+  const uint8_t paletteIndex = setEffectPacket->readPaletteIndexFromSetEffect();
+  // Rough check for whether the entire palette is the same color - if so,
+  // change the brightness rather than the hue.
+  if (ColorFromPalette(palettes[paletteIndex], 0) ==
+      ColorFromPalette(palettes[paletteIndex], 64)) {
+    // Solid color palette
+    if (numLeds < 8) {
+      return ColorFromPalette(palettes[paletteIndex], 0,
+                              cubicwave8(timeMs / 16));
+    } else {
+      return ColorFromPalette(
+          palettes[paletteIndex], 0,
+          (cubicwave8(timeMs / 16 + ledIndex * 8) * (uint16_t)2) / 3);
+    }
   } else {
-    return ColorFromPalette(
-        palettes[setEffectPacket->readPaletteIndexFromSetEffect()],
-        timeMs / 16 + ledIndex * 8, 128);
+    // Varying color palette
+    if (numLeds < 8) {
+      return ColorFromPalette(palettes[paletteIndex], timeMs / 16, 128);
+    } else {
+      return ColorFromPalette(palettes[paletteIndex],
+                              timeMs / 16 + ledIndex * 8, 128);
+    }
   }
 }
