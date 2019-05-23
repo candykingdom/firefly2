8c2cda38854c44943a5c4811063d8bf9ce499063
Adam Demuri <adam.demuri@gmail.com>
2019-05-22 22:06:38 -0600

    Add a simple blink effect
    Change-Id: I073d481ebeaf7c8afe32523d6543eec4e28f30ab

---
+++ software/generic/SimpleBlinkEffect.hpp
@@ -0,0 +1,18 @@
+#ifndef __SIMPLE_BLINK_EFFECT_HPP__
+#define __SIMPLE_BLINK_EFFECT_HPP__
+
+#include "Effect.hpp"
+#include "Types.hpp"
+
+/** Cycles all of the LEDs through the color palette at once. */
+class SimpleBlinkEffect : public Effect {
+ public:
+  SimpleBlinkEffect(uint8_t numLeds);
+
+  /** Gets the value of a specific LED at a specific time. */
+  CRGB GetRGB(uint8_t ledIndex, uint32_t timeMs,
+              RadioPacket *setEffectPacket) override;
+
+ private:
+};
+#endif
