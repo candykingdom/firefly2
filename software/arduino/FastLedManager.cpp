56edaf42ab82ec1b20e3d991be738f5a1b0f5a88
Adam Demuri <adam.demuri@gmail.com>
2019-06-04 12:48:59 -0600

    Productionizing:
    - Mirror the first LED, so that the pattern begins with the first LED on
    the strip, not the board.
    - Reduce startup delay and don't flash yellow at boot
    
    Change-Id: Ie30a496d280678bc0be8ad65502c68f82c0ed38f

--- software/arduino/FastLedManager.cpp
+++ software/arduino/FastLedManager.cpp
@@ -3,8 +3,9 @@
 FastLedManager::FastLedManager(const uint8_t numLeds,
                                RadioStateMachine *radioState)
     : LedManager(numLeds, radioState) {
-  leds = new CRGB[numLeds];
-  FastLED.addLeds<NEOPIXEL, WS2812_PIN>(leds, numLeds)
+  // The first LED is on-board, and should mirror the first LED of the strip.
+  leds = new CRGB[numLeds + 1];
+  FastLED.addLeds<NEOPIXEL, WS2812_PIN>(leds, numLeds + 1)
       .setCorrection(TypicalLEDStrip);
   FastLED.showColor(CRGB(0, 0, 0));
 }
@@ -12,7 +13,11 @@ FastLedManager::FastLedManager(const uint8_t numLeds,
 void FastLedManager::SetGlobalColor(CRGB rgb) { FastLED.showColor(rgb); }
 
 void FastLedManager::SetLed(uint8_t ledIndex, CRGB *const rgb) {
-  leds[ledIndex] = *rgb;
+  // The first LED is on-board, and should mirror the first LED of the strip.
+  if (ledIndex == 0) {
+    leds[0] = *rgb;
+  }
+  leds[ledIndex + 1] = *rgb;
 }
 
 void FastLedManager::WriteOutLeds() { FastLED.show(); }
