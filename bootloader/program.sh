32c5d8916654b846e92faab94b91e040ea2e144b
Adam Demuri <adam.demuri@gmail.com>
2019-09-20 19:44:50 -0600

    WIP enable the watchdog timer.
    This enables a timer which will reset the board if it has not 'fed' the
    watchdog in ~100ms. The main loop feeds the watchdog, so the board
    should reset if it hangs.
    
    This also updates the bootloader to disable the watchdog. This is very
    important - without this update, a board with a bad program may be
    unprogrammable over USB until an updated bootloader is flashed via
    openocd.
    
    Change-Id: Ia8e93f49543822d839f89a6a51f101713bd44310

--- bootloader/program.sh
+++ bootloader/program.sh
@@ -1,6 +1,6 @@
 cat - | nc localhost 4444 <<EOF
 reset halt
-flash write_image erase bootloader-rfboard-v3.5.0-21-gfb6fecf.bin
+flash write_image erase bootloader-rfboard-v3.6.0-48-g2ae51ff.bin
 reset run
 exit
 
--- software/arduino/node/node.ino
+++ software/arduino/node/node.ino
@@ -19,6 +19,8 @@ NetworkManager* nm;
 FastLedManager* ledManager;
 RadioStateMachine* stateMachine;
 
+void FeedWatchdog() { WDT->CLEAR.reg = WDT_CLEAR_CLEAR_KEY; }
+
 void setup() {
   Serial.begin(115200);
   pinMode(kLedPin, OUTPUT);
@@ -28,10 +30,58 @@ void setup() {
   stateMachine = new RadioStateMachine(nm);
 
   ledManager = new FastLedManager(kNumLeds, deviceType, stateMachine);
+  // NOTE: can check if we watchdog rebooted by checking REG_PM_RCAUSE
+  // See https://github.com/gjt211/SAMD21-Reset-Cause
+
+  // TODO: remove blink
+  ledManager->SetGlobalColor(CRGB(CRGB::White));
+  delay(20);
   ledManager->SetGlobalColor(CRGB(CRGB::Black));
+
+  // Set up the watchdog timer: this will reset the processor if it hasn't
+  // 'fed' the watchdog in ~100ms.
+  // Inspired by
+  // https://github.com/adafruit/Adafruit_SleepyDog/blob/master/utility/WatchdogSAMD.cpp
+  // Disable watchdog and wait for it to take effect
+  WDT->CTRL.reg = 0;
+  while (WDT->STATUS.bit.SYNCBUSY) {
+  }
+
+  // Configure the watchdog clock
+  // Note: feeding the watchdog is slightly expensive - it seems to take O(1
+  // tick of the oscillator). So, don't use a prescaler. I tried using a /32
+  // prescaler, and the animations were horribly slow.
+  // Generic clock generator 2, no divisor
+  GCLK->GENDIV.reg = GCLK_GENDIV_ID(2);
+  // Enable clock generator 2 using low-power 32KHz oscillator.
+  GCLK->GENCTRL.reg =
+      GCLK_GENCTRL_ID(2) | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSCULP32K;
+  while (GCLK->STATUS.bit.SYNCBUSY) {
+  }
+  // WDT clock = clock gen 2
+  GCLK->CLKCTRL.reg =
+      GCLK_CLKCTRL_ID_WDT | GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK2;
+
+  // Now, configure
+  WDT->INTENCLR.bit.EW = 1;  // Disable early warning interrupt
+  // Documentation for this field here:
+  // http://asf.atmel.com/docs/latest/samd20/html/group__asfdoc__sam0__wdt__group.html
+  WDT->CONFIG.bit.PER =
+      9;  // WDT clock runs at ~32kHz (effective). A value of 9 means 4096
+          // cycles, which means a period of ~128ms.
+  WDT->CTRL.bit.WEN = 0;  // Disable window mode
+  while (WDT->STATUS.bit.SYNCBUSY) {
+  }
+
+  FeedWatchdog();
+  // Now, enable the watchdog
+  WDT->CTRL.bit.ENABLE = 1;
+  while (WDT->STATUS.bit.SYNCBUSY) {
+  }
 }
 
 unsigned long printAliveAt = 0;
+uint8_t watchdog_counter = 0;
 
 void loop() {
   stateMachine->Tick();
@@ -41,4 +91,13 @@ void loop() {
     Serial.println(stateMachine->GetNetworkMillis());
     printAliveAt = millis() + 1000;
   }
+
+  // Feeding the watchdog takes some time. Each run through this loop should
+  // take less than 10ms, so it should be safe to feed the watchdog every 5
+  // cycles = ~50ms.
+  if (watchdog_counter > 5) {
+    FeedWatchdog();
+    watchdog_counter = 0;
+  }
+  watchdog_counter++;
 }
