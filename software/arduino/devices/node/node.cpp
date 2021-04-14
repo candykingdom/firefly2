2c130688d5527652209f457ce0a61474bf92a869
Will Martin <williammartinthird@gmail.com>
2021-04-13 22:34:52 -0600

    Rearrange devices
    Move device specific code into their own folders. This allows us to
    exclude everything but the device we want to target in the
    platformio.ini file.

--- software/arduino/devices/node/node.cpp
+++ software/arduino/devices/node/node.cpp
@@ -2,10 +2,10 @@
 // (which it shouldn't)
 #undef max
 #undef min
-#include "../../generic/NetworkManager.hpp"
-#include "../../generic/RadioStateMachine.hpp"
-#include "../FastLedManager.hpp"
-#include "../RadioHeadRadio.hpp"
+#include "../../FastLedManager.hpp"
+#include "../../../generic/NetworkManager.hpp"
+#include "../../RadioHeadRadio.hpp"
+#include "../../../generic/RadioStateMachine.hpp"
 
 const int kLedPin = 0;
 const int kNumLeds = 30;  // Bike
