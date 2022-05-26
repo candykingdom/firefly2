a1f646c669397ff76467e62b3beff59198d56fa6
Will Martin <williammartinthird@gmail.com>
2022-05-25 23:02:23 -0600

    Update Contrast Bump for circular devices
    This splits the device into 2 even halves and runs the effect on each.

--- software/generic/ContrastBumpsEffect.cpp
+++ software/generic/ContrastBumpsEffect.cpp
@@ -7,8 +7,15 @@ CRGB ContrastBumpsEffect::GetRGB(uint8_t ledIndex, uint32_t timeMs,
                                  RadioPacket *setEffectPacket) {
   const uint8_t paletteIndex = setEffectPacket->readPaletteIndexFromSetEffect();
   ColorPalette palette = palettes[paletteIndex];
+  
+  uint8_t offset; 
+  if (device->FlagEnabled(Circular)) {
+    offset = (uint16_t)ledIndex * 255 / device->led_count;
+  } else {
+    offset = ledIndex * 24;
+  }
 
-  uint8_t sin = GetThresholdSin(-(timeMs / 16 + ledIndex * 24) << 8, 0);
+  uint8_t sin = GetThresholdSin(-(timeMs / 16 + offset) << 8, 0);
   uint16_t colorIndex = timeMs * 16;
   if (sin == 0) {
     CHSV color = palette.GetGradient(colorIndex);
