f9c5aa4a678634a02bd9f5924c7ac1dc7a68e723
Will Martin <williammartinthird@gmail.com>
2022-05-28 13:35:38 -0600

    Move Math.hpp into Math.cpp

--- software/generic/Math.hpp
+++ software/generic/Math.hpp
@@ -14,11 +14,6 @@
  * Perlin noise functoins.
  */
 void GetPosOnCircle(uint8_t led_count, uint8_t led_index, uint8_t *angle,
-                    uint8_t *radius) {
-  uint32_t tau = 6283;
-  uint32_t circum = led_count * 32;
-  *angle = led_index * 255 / led_count;
-  *radius = (circum * 1000) / tau;
-}
+                    uint8_t *radius);
 
 #endif  // __MATH_HPP__
