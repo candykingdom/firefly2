429f9c6828658386f329cefd82f292085b415ea2
Will Martin <williammartinthird@gmail.com>
2022-05-29 12:38:38 -0600

    Fix linter

--- lib/storage/Storage.hpp
+++ lib/storage/Storage.hpp
@@ -4,16 +4,18 @@
 #include "../device/DeviceDescription.hpp"
 
 /**
- * @brief Retrieves the DeviceDescription from storage and returns a boolean if reading was successful.
- * 
+ * @brief Retrieves the DeviceDescription from storage and returns a boolean if
+ * reading was successful.
+ *
  * @param device A pointer to write the DeviceDescription into.
  * @return If retrieving the DeviceDescription was successful.
  */
 bool GetDeviceDescription(const DeviceDescription* device);
 
 /**
- * @brief Stores the DeviceDescription to storage and returns a boolean if reading was successful.
- * 
+ * @brief Stores the DeviceDescription to storage and returns a boolean if
+ * reading was successful.
+ *
  * @param device A pointer to the DeviceDescription to store.
  * @return If storing the DeviceDescription was successful.
  */
