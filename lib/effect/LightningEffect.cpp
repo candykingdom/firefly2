aea90622219da758aee26c25c49ba5bf34d12033
Will Martin <williammartinthird@gmail.com>
2022-07-17 22:04:43 -0600

    Fix left-shift errors
    We were trying to left shift negative numbers. This fixes smalltests so
    we no longer try to left shift any negative numbers.
    
    Fixes #37

--- lib/effect/LightningEffect.cpp
+++ lib/effect/LightningEffect.cpp
@@ -14,7 +14,7 @@ CRGB LightningEffect::GetRGB(uint8_t led_index, uint32_t time_ms,
   ColorPalette palette = palettes[palette_index];
 
   uint8_t noise = perlinNoise(led_index << 6, time_ms / 6);
-  CHSV color = palette.GetGradient(((noise - 160) << 9) + time_ms);
+  CHSV color = palette.GetGradient(((noise - 160) * 512) + time_ms);
   if (noise > 160) {
     color.v = noise;
   } else {
