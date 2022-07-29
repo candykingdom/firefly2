ecca81f32938304feb95d270c27b029b1b821614
Adam DeMuri <adam.demuri@gmail.com>
2022-07-28 22:49:32 -0600

    Port main code to fancy-node
    Change-Id: I9eaae5c850bb63e08b0cb4d17d84f0f9cfa33465

--- src/devices/fancy-node/fancy-node.cc
+++ src/devices/fancy-node/fancy-node.cc
@@ -1,19 +1,50 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <DeviceDescription.hpp>
+#include <StripDescription.hpp>
+#include <vector>
+
+#include "../../arduino/FastLedManager.hpp"
+#include "../../arduino/RadioHeadRadio.hpp"
+#include "../../generic/NetworkManager.hpp"
+#include "../../generic/RadioStateMachine.hpp"
 
 const int kLedPin = PB0;
 const int kNeopixelPin = PA12;
-CRGB leds[2];
+
+static const DeviceDescription *SimpleRfBoardDescription(
+    uint8_t led_count, std::vector<StripFlag> flags) {
+  return new DeviceDescription(2000,
+                               {
+                                   new StripDescription(led_count, flags),
+                               });
+}
+
+const DeviceDescription *const test_device = SimpleRfBoardDescription(1, {Bright});
+
+RadioHeadRadio *radio;
+NetworkManager *nm;
+FastLedManager *led_manager;
+RadioStateMachine *state_machine;
 
 void setup() {
   pinMode(kLedPin, OUTPUT);
   digitalWrite(kLedPin, HIGH);
 
-  FastLED.addLeds<NEOPIXEL, kNeopixelPin>(leds, 2);
+
+  SPI.setMISO(PA6);
+  SPI.setMOSI(PA7);
+  SPI.setSCLK(PA5);
+  SPI.setSSEL(PA4);
+  radio = new RadioHeadRadio();
+  nm = new NetworkManager(radio);
+  state_machine = new RadioStateMachine(nm);
+
+  led_manager = new FastLedManager(test_device, state_machine);
+  led_manager->PlayStartupAnimation();
 }
 
 void loop() {
-  FastLED.showColor(CHSV(millis() / 10, 255, 255));
-  digitalWrite(kLedPin, (millis() / 200) % 2);
-  delay(10);
+  state_machine->Tick();
+  led_manager->RunEffect();
 }
