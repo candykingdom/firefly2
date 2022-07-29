45615756a1c2a9001e94296caf2b841514b0ab49
Adam DeMuri <adam.demuri@gmail.com>
2022-07-28 22:48:23 -0600

    Update range test
    Change-Id: I670a38ee36555c1f0813b679cda9b9fbc306a0ae

--- src/devices/range_test/range_test.cc
+++ src/devices/range_test/range_test.cc
@@ -1,26 +1,37 @@
+#include <Arduino.h>
+
+
 #undef max
 #undef min
+#include <DeviceDescription.hpp>
+#include <StripDescription.hpp>
 #include "../../arduino/FastLedManager.hpp"
 #include "../../arduino/RadioHeadRadio.hpp"
 #include "../../generic/NetworkManager.hpp"
 #include "../../generic/RadioStateMachine.hpp"
 
-const int kNumLeds = 60;
+constexpr int kLedPin = 0;
+static const DeviceDescription device{200, {new StripDescription(1, {})}};
 
 RadioHeadRadio* radio;
-FastLedManager* ledManager;
+NetworkManager *nm;
+RadioStateMachine *state_machine;
+FastLedManager* led_manager;
 
 RadioPacket packetToSend;
 void setup() {
+  pinMode(kLedPin, OUTPUT);
   Serial.begin(115200);
   // Delay makes it easier to reset the board in case of failure
   delay(1000);
 
-  ledManager = new FastLedManager(kNumLeds);
-  // Yellow LED on boot indicates a problem initializing the radio
-  ledManager->SetGlobalColor(CHSV(HUE_YELLOW, 255, 128));
   radio = new RadioHeadRadio();
-  ledManager->SetGlobalColor(CRGB(CRGB::Black));
+  nm = new NetworkManager(radio);
+  state_machine = new RadioStateMachine(nm);
+  led_manager = new FastLedManager(&device, state_machine);
+  // Yellow LED on boot indicates a problem initializing the radio
+  led_manager->SetGlobalColor(CHSV(HUE_YELLOW, 255, 128));
+  led_manager->SetGlobalColor(CRGB(CRGB::Black));
 
   packetToSend.writeSetEffect(0, 10, 4);
 }
@@ -28,6 +39,7 @@ void setup() {
 RadioPacket packet;
 uint32_t receivedPacketAt = 0;
 void loop() {
+  digitalWrite(kLedPin, (millis() / 200) % 2);
   bool receivedPacket = false;
   uint32_t origMillis = millis();
   while ((millis() - origMillis) < 5) {
@@ -42,15 +54,15 @@ void loop() {
     // RSSI range is -120 to 0 (0 is best), so this maps from aqua (high
     // strength) to red (low strength)
     uint8_t hue = 120 + (radio->LastRssi());
-    ledManager->SetGlobalColor(CHSV(hue, 255, 255));
+    led_manager->SetGlobalColor(CHSV(hue, 255, 255));
 
     // Wait for the other radio to transition to receiving
     delay(1);
-    radio->packetToSend(packetToSend);
+    radio->sendPacket(packetToSend);
   } else {
-    radio->packetToSend(packetToSend);
+    radio->sendPacket(packetToSend);
     if (millis() - receivedPacketAt > 500) {
-      ledManager->SetGlobalColor(CRGB(CRGB::Black));
+      led_manager->SetGlobalColor(CRGB(CRGB::Black));
     }
   }
 }
