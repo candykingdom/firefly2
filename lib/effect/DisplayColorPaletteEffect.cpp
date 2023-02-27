f89fa4031f275b93b5da138b08c0e3f77d037e9c
Adam DeMuri <adam.demuri@gmail.com>
2023-02-26 22:02:18 -0700

    Display color palettes across multiple LEDs
    Change-Id: Iaa52527453ee5a9bce5b9dc3ffe2a3ef575b9354

--- lib/effect/DisplayColorPaletteEffect.cpp
+++ lib/effect/DisplayColorPaletteEffect.cpp
@@ -5,10 +5,15 @@ DisplayColorPaletteEffect::DisplayColorPaletteEffect() : Effect() {}
 CRGB DisplayColorPaletteEffect::GetRGB(uint8_t led_index, uint32_t time_ms,
                                        const StripDescription &strip,
                                        RadioPacket *setEffectPacket) {
-  UNUSED(led_index);
   ColorPalette palette =
       palettes()[setEffectPacket->readPaletteIndexFromSetEffect()];
-  CHSV color = palette.GetGradient((time_ms / 2) * 23);
+  CHSV color;
+  if (strip.led_count < palette.Size() && palette.Size() <= 4) {
+    color = palette.GetGradient((time_ms / 2) * 23);
+  } else {
+    color = palette.GetGradient((((uint32_t)led_index) * 65536) / strip.led_count);
+  }
+
   if (!strip.FlagEnabled(Bright)) {
     color.v /= 2;
   }
