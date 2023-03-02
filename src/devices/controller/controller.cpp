061eb05b358930cf062c945c8a39c88ce93deeb6
Adam DeMuri <adam.demuri@gmail.com>
2023-03-01 18:39:33 -0700

    Implement controller color mode
    Change-Id: I2dfcc9af9dace32dcfde54d031b3ba45cafe1ab2

--- src/devices/controller/controller.cpp
+++ src/devices/controller/controller.cpp
@@ -8,6 +8,20 @@
 #include "generic/NetworkManager.hpp"
 #include "generic/RadioStateMachine.hpp"
 
+// Which mode of control the device is in
+enum class ControllerMode {
+  // Choose an effect and a color palette.
+  Effect,
+
+  // Left/right buttons immediately make everything light up a single color.
+  DirectColor,
+
+  // Left/right buttons immediately change the palette for the current effect.
+  DirectPalette,
+};
+
+ControllerMode mode = ControllerMode::DirectColor;
+
 // Pin definitions
 constexpr int kSwitchLeft = PA0;
 constexpr int kSwitchRight = PA1;
@@ -44,32 +58,27 @@ DisplayColorPaletteEffect palette_effect;
 uint8_t effect_index = 0;
 uint8_t palette_index = 0;
 RadioPacket set_effect_packet;
+RadioPacket control_packet;
 
 AnalogButton left_buttons(kSwitchLeft);
 AnalogButton right_buttons(kSwitchRight);
 AnalogButton bottom_buttons(kSwitchBottom);
 
-void setup() {
-  FastLED.addLeds<NEOPIXEL, kNeopixelPin>(leds, kLedCount)
-      .setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(32);
+// Colors for color mode
+std::array<CHSV, 6> colors = {
+    CHSV{0, 255, 255},           CHSV{256 / 6, 255, 255},
+    CHSV{256 * 2 / 6, 255, 255}, CHSV{256 * 3 / 6, 255, 255},
+    CHSV{256 * 4 / 6, 255, 255}, CHSV{256 * 5 / 6, 255, 255}};
 
-  if (!radio.Begin()) {
-    while (true) {
-      leds[0] = CRGB((millis() / 200) % 2 == 0 ? 255 : 0, 0, 0);
-      FastLED.show();
-    }
+void SetMainLed(uint8_t led_index, CRGB rgb) {
+  if (led_index > 11 && led_index < 24) {
+    leds[12 + (23 - led_index)] = rgb;
+  } else {
+    leds[led_index] = rgb;
   }
-  FastLED.clear(/*writeData=*/true);
 }
 
-void loop() {
-  state_machine.Tick();
-
-  left_buttons.Tick();
-  right_buttons.Tick();
-  bottom_buttons.Tick();
-
+void RunEffectMode() {
   const uint8_t num_unique_effects = led_manager.GetNumUniqueEffects();
   const uint8_t num_palettes = Effect::palettes().size();
   if (left_buttons.Button1Pressed()) {
@@ -93,14 +102,15 @@ void loop() {
 
   // Update the top row with the effect output.
   for (uint8_t i = 0; i < kRowStrip.led_count; i++) {
-    leds[i] = current_effect->GetRGB(i, state_machine.GetNetworkMillis(),
-                                     kRowStrip, &set_effect_packet);
+    SetMainLed(i, current_effect->GetRGB(i, state_machine.GetNetworkMillis(),
+                                         kRowStrip, &set_effect_packet));
   }
 
   // Update the middle row with the palette colors.
   for (uint8_t i = 0; i < kRowStrip.led_count; i++) {
-    leds[i + kRowStrip.led_count] = palette_effect.GetRGB(
-        i, state_machine.GetNetworkMillis(), kRowStrip, &set_effect_packet);
+    SetMainLed(i + kRowStrip.led_count,
+               palette_effect.GetRGB(i, state_machine.GetNetworkMillis(),
+                                     kRowStrip, &set_effect_packet));
   }
 
   // Update slave status LED.
@@ -110,7 +120,79 @@ void loop() {
 
   // Update "sending" status LED.
   leds[kStatusRight] = CRGB(0, 0, broadcast_led_timer.Active() ? 255 : 0);
-  FastLED.show();
+}
 
+void RunColorMode() {
+  for (uint8_t i = 0; i < 36; i++) {
+    if ((i % 12) > 4 && (i % 12) <= 6) {
+      SetMainLed(i, CRGB(0, 0, 0));
+    } else {
+      SetMainLed(i, colors[(i / 6)]);
+    }
+  }
+
+  bool pressed = false;
+  uint8_t color_index = 0;
+  if (left_buttons.Button1Pressed()) {
+    pressed = true;
+    color_index = 0;
+  } else if (left_buttons.Button2Pressed()) {
+    pressed = true;
+    color_index = 2;
+  } else if (left_buttons.Button3Pressed()) {
+    pressed = true;
+    color_index = 4;
+  } else if (right_buttons.Button1Pressed()) {
+    pressed = true;
+    color_index = 1;
+  } else if (right_buttons.Button2Pressed()) {
+    pressed = true;
+    color_index = 3;
+  } else if (right_buttons.Button3Pressed()) {
+    pressed = true;
+    color_index = 5;
+  }
+
+  if (pressed) {
+    control_packet.writeControl(kSetEffectDelay, colors[color_index]);
+    state_machine.SetEffect(&control_packet);
+  }
+}
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, kNeopixelPin>(leds, kLedCount)
+      .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(32);
+
+  if (!radio.Begin()) {
+    while (true) {
+      leds[0] = CRGB((millis() / 200) % 2 == 0 ? 255 : 0, 0, 0);
+      FastLED.show();
+    }
+  }
+  FastLED.clear(/*writeData=*/true);
+}
+
+void loop() {
+  state_machine.Tick();
+
+  left_buttons.Tick();
+  right_buttons.Tick();
+  bottom_buttons.Tick();
+
+  // TODO(adam): choose the mode based off of the mode switch, once it's
+  // electrically connected to the MCU (d'oh)
+
+  switch (mode) {
+    case ControllerMode::Effect:
+      RunEffectMode();
+      break;
+
+    case ControllerMode::DirectColor:
+      RunColorMode();
+      break;
+  }
+
+  FastLED.show();
   delay(5);
 }
