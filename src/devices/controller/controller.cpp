abe0daf1a83624f209200f9625e9002915db86f4
Adam DeMuri <adam.demuri@gmail.com>
2023-04-19 11:42:54 -0600

    Clear nBOOT_SEL in controller init
    Change-Id: I989357d6fb6e667d7862bb395940f685f17b647b

--- src/devices/controller/controller.cpp
+++ src/devices/controller/controller.cpp
@@ -312,6 +312,26 @@ void RunPaletteMode() {
 }
 
 void setup() {
+  // check if nBOOT_SEL bit is set
+  if (FLASH->OPTR & FLASH_OPTR_nBOOT_SEL) {
+    // unlock flash/option
+    FLASH->KEYR = 0x45670123;
+    FLASH->KEYR = 0xCDEF89AB;
+    FLASH->OPTKEYR = 0x08192A3B;
+    FLASH->OPTKEYR = 0x4C5D6E7F;
+
+    while (FLASH->SR & FLASH_SR_BSY1)
+      ;
+
+    // clear nBOOT_SEL bit
+    FLASH->OPTR &= ~FLASH_OPTR_nBOOT_SEL;
+
+    // write
+    FLASH->CR |= FLASH_CR_OPTSTRT;
+    while (FLASH->SR & FLASH_SR_BSY1)
+      ;
+  }
+
   FastLED.addLeds<NEOPIXEL, kNeopixelPin>(leds.data(), kLedCount)
       .setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(32);
