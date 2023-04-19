c31d0a11d90879457dd405a4a52669c214a59ac5
Adam DeMuri <adam.demuri@gmail.com>
2023-04-18 20:15:51 -0600

    Migrate controller to stm32g070 for board v1.1
    Change-Id: I6f088b3873b6a80aea1802de9a87b9831a5f6d1f

--- src/devices/controller/controller.cpp
+++ src/devices/controller/controller.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <FastLED.h>
 #include <arduino-timer.h>
+#include <debounce-filter.h>
 #include <median-filter.h>
+
+#include <array>
 
 #include "FakeLedManager.hpp"
 #include "analog-button.h"
@@ -28,14 +31,16 @@ ControllerMode mode = ControllerMode::Effect;
 ControllerMode prev_mode = mode;
 
 // Pin definitions
-constexpr int kSwitchLeft = PA0;
-constexpr int kSwitchRight = PA1;
-constexpr int kSwitchBottom = PA2;
+constexpr std::array<uint8_t, 3> kLeftButtons = {PB2, PB14, PB15};
+constexpr std::array<uint8_t, 3> kRightButtons = {PA8, PC6, PC7};
+constexpr std::array<uint8_t, 3> kBottomButtons = {PA11, PA12, PA15};
 
-constexpr int kVbattDiv = PA3;
-constexpr int kModeSwitch = PB1;
+constexpr int kModeSwitch = PA6;
+constexpr int kVbattDiv = PA7;
 
-constexpr int kNeopixelPin = PB15;
+// Note: for some reason, `PA4` has a resolved pin number of 196, which confuses
+// FastLED.
+constexpr int kNeopixelPin = PA_4;
 
 const StripDescription kRowStrip =
     StripDescription(/*led_count=*/12, {Bright, Controller});
@@ -67,9 +72,23 @@ uint8_t palette_index = 0;
 RadioPacket set_effect_packet;
 RadioPacket control_packet;
 
-AnalogButton left_buttons(kSwitchLeft);
-AnalogButton right_buttons(kSwitchRight);
-AnalogButton bottom_buttons(kSwitchBottom);
+std::array<DebounceFilter, 3> left_buttons = {
+    DebounceFilter(filter_functions::ForDigitalRead<kLeftButtons[0]>()),
+    DebounceFilter(filter_functions::ForDigitalRead<kLeftButtons[1]>()),
+    DebounceFilter(filter_functions::ForDigitalRead<kLeftButtons[2]>()),
+};
+
+std::array<DebounceFilter, 3> right_buttons = {
+    DebounceFilter(filter_functions::ForDigitalRead<kRightButtons[0]>()),
+    DebounceFilter(filter_functions::ForDigitalRead<kRightButtons[1]>()),
+    DebounceFilter(filter_functions::ForDigitalRead<kRightButtons[2]>()),
+};
+
+std::array<DebounceFilter, 3> bottom_buttons = {
+    DebounceFilter(filter_functions::ForDigitalRead<kBottomButtons[0]>()),
+    DebounceFilter(filter_functions::ForDigitalRead<kBottomButtons[1]>()),
+    DebounceFilter(filter_functions::ForDigitalRead<kBottomButtons[2]>()),
+};
 
 MedianFilter<uint16_t, uint16_t, 5> mode_switch(
     filter_functions::ForAnalogRead<kModeSwitch>());
@@ -89,36 +108,36 @@ std::array<uint8_t, 6> palettes = {
 void RunEffectMode() {
   const uint8_t num_unique_effects = led_manager.GetNumUniqueEffects();
   const uint8_t num_palettes = Effect::palettes().size();
-  if (left_buttons.Button1Pressed()) {
+  if (left_buttons[0].Rose()) {
     effect_index = (effect_index - 1 + num_unique_effects) % num_unique_effects;
-  } else if (right_buttons.Button1Pressed()) {
+  } else if (right_buttons[0].Rose()) {
     effect_index = (effect_index + 1) % num_unique_effects;
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     palette_index = (palette_index - 1 + num_palettes) % num_palettes;
-  } else if (right_buttons.Button2Pressed()) {
+  } else if (right_buttons[1].Rose()) {
     palette_index = (palette_index + 1) % num_palettes;
-  } else if (bottom_buttons.Button1Pressed()) {
+  } else if (bottom_buttons[0].Rose()) {
     state_machine.SetEffect(&set_effect_packet);
     broadcast_led_timer.Reset();
   }
 
-  if (left_buttons.Button1Pressed()) {
+  if (left_buttons[0].Rose()) {
     SetLeftButtonLeds(kButtonPressedBrightness, kButtonActiveBrightness, 0);
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     SetLeftButtonLeds(kButtonActiveBrightness, kButtonPressedBrightness, 0);
   } else {
     SetLeftButtonLeds(kButtonActiveBrightness, kButtonActiveBrightness, 0);
   }
 
-  if (right_buttons.Button1Pressed()) {
+  if (right_buttons[0].Rose()) {
     SetRightButtonLeds(kButtonPressedBrightness, kButtonActiveBrightness, 0);
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     SetRightButtonLeds(kButtonActiveBrightness, kButtonPressedBrightness, 0);
   } else {
     SetRightButtonLeds(kButtonActiveBrightness, kButtonActiveBrightness, 0);
   }
 
-  if (bottom_buttons.Button1Pressed()) {
+  if (bottom_buttons[0].Rose()) {
     SetBottomButtonLeds(kButtonPressedBrightness, 0, 0);
   } else {
     SetBottomButtonLeds(kButtonActiveBrightness, 0, 0);
@@ -163,33 +182,33 @@ void RunColorMode() {
 
   bool pressed = false;
   uint8_t color_index = 0;
-  if (left_buttons.Button1Pressed()) {
+  if (left_buttons[0].Rose()) {
     pressed = true;
     color_index = 0;
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     pressed = true;
     color_index = 2;
-  } else if (left_buttons.Button3Pressed()) {
+  } else if (left_buttons[2].Rose()) {
     pressed = true;
     color_index = 4;
-  } else if (right_buttons.Button1Pressed()) {
+  } else if (right_buttons[0].Rose()) {
     pressed = true;
     color_index = 1;
-  } else if (right_buttons.Button2Pressed()) {
+  } else if (right_buttons[1].Rose()) {
     pressed = true;
     color_index = 3;
-  } else if (right_buttons.Button3Pressed()) {
+  } else if (right_buttons[2].Rose()) {
     pressed = true;
     color_index = 5;
   }
 
-  if (left_buttons.Button1Pressed()) {
+  if (left_buttons[0].Rose()) {
     SetLeftButtonLeds(kButtonPressedBrightness, kButtonActiveBrightness,
                       kButtonActiveBrightness);
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     SetLeftButtonLeds(kButtonActiveBrightness, kButtonPressedBrightness,
                       kButtonActiveBrightness);
-  } else if (left_buttons.Button3Pressed()) {
+  } else if (left_buttons[2].Rose()) {
     SetLeftButtonLeds(kButtonActiveBrightness, kButtonActiveBrightness,
                       kButtonPressedBrightness);
   } else {
@@ -197,13 +216,13 @@ void RunColorMode() {
                       kButtonActiveBrightness);
   }
 
-  if (right_buttons.Button1Pressed()) {
+  if (right_buttons[0].Rose()) {
     SetRightButtonLeds(kButtonPressedBrightness, kButtonActiveBrightness,
                        kButtonActiveBrightness);
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     SetRightButtonLeds(kButtonActiveBrightness, kButtonPressedBrightness,
                        kButtonActiveBrightness);
-  } else if (left_buttons.Button3Pressed()) {
+  } else if (left_buttons[2].Rose()) {
     SetRightButtonLeds(kButtonActiveBrightness, kButtonActiveBrightness,
                        kButtonPressedBrightness);
   } else {
@@ -235,33 +254,33 @@ void RunPaletteMode() {
 
   bool pressed = false;
   uint8_t palette_index = 0;
-  if (left_buttons.Button1Pressed()) {
+  if (left_buttons[0].Rose()) {
     pressed = true;
     palette_index = 0;
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     pressed = true;
     palette_index = 2;
-  } else if (left_buttons.Button3Pressed()) {
+  } else if (left_buttons[2].Rose()) {
     pressed = true;
     palette_index = 4;
-  } else if (right_buttons.Button1Pressed()) {
+  } else if (right_buttons[0].Rose()) {
     pressed = true;
     palette_index = 1;
-  } else if (right_buttons.Button2Pressed()) {
+  } else if (right_buttons[1].Rose()) {
     pressed = true;
     palette_index = 3;
-  } else if (right_buttons.Button3Pressed()) {
+  } else if (right_buttons[2].Rose()) {
     pressed = true;
     palette_index = 5;
   }
 
-  if (left_buttons.Button1Pressed()) {
+  if (left_buttons[0].Rose()) {
     SetLeftButtonLeds(kButtonPressedBrightness, kButtonActiveBrightness,
                       kButtonActiveBrightness);
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (left_buttons[1].Rose()) {
     SetLeftButtonLeds(kButtonActiveBrightness, kButtonPressedBrightness,
                       kButtonActiveBrightness);
-  } else if (left_buttons.Button3Pressed()) {
+  } else if (left_buttons[2].Rose()) {
     SetLeftButtonLeds(kButtonActiveBrightness, kButtonActiveBrightness,
                       kButtonPressedBrightness);
   } else {
@@ -269,13 +288,13 @@ void RunPaletteMode() {
                       kButtonActiveBrightness);
   }
 
-  if (right_buttons.Button1Pressed()) {
+  if (right_buttons[0].Rose()) {
     SetRightButtonLeds(kButtonPressedBrightness, kButtonActiveBrightness,
                        kButtonActiveBrightness);
-  } else if (left_buttons.Button2Pressed()) {
+  } else if (right_buttons[1].Rose()) {
     SetRightButtonLeds(kButtonActiveBrightness, kButtonPressedBrightness,
                        kButtonActiveBrightness);
-  } else if (left_buttons.Button3Pressed()) {
+  } else if (right_buttons[2].Rose()) {
     SetRightButtonLeds(kButtonActiveBrightness, kButtonActiveBrightness,
                        kButtonPressedBrightness);
   } else {
@@ -304,14 +323,30 @@ void setup() {
     }
   }
   FastLED.clear(/*writeData=*/true);
+
+  pinMode(kLeftButtons[0], INPUT_PULLUP);
+  pinMode(kLeftButtons[0], INPUT_PULLUP);
+  pinMode(kLeftButtons[1], INPUT_PULLUP);
+  pinMode(kRightButtons[0], INPUT_PULLUP);
+  pinMode(kRightButtons[1], INPUT_PULLUP);
+  pinMode(kRightButtons[2], INPUT_PULLUP);
+  pinMode(kBottomButtons[2], INPUT_PULLUP);
+  pinMode(kBottomButtons[1], INPUT_PULLUP);
+  pinMode(kBottomButtons[2], INPUT_PULLUP);
 }
 
 void loop() {
   state_machine.Tick();
 
-  left_buttons.Tick();
-  right_buttons.Tick();
-  bottom_buttons.Tick();
+  left_buttons[0].Run();
+  left_buttons[1].Run();
+  left_buttons[2].Run();
+  right_buttons[0].Run();
+  right_buttons[1].Run();
+  right_buttons[2].Run();
+  bottom_buttons[0].Run();
+  bottom_buttons[1].Run();
+  bottom_buttons[2].Run();
 
   mode_switch.Run();
   if (mode_switch.GetFilteredValue() < kMode1Threshold) {
