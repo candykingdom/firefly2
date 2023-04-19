6ddb69fbb23ebf6ff0dd3258bc076eece657b5e9
Adam DeMuri <adam.demuri@gmail.com>
2023-04-19 12:57:34 -0600

    Clear nBOOT_SEL in fancy-node init
    Change-Id: Id8cb63b6ce848cc6eded9adadc20416ba0585423

--- src/devices/fancy-node/fancy-node.cc
+++ src/devices/fancy-node/fancy-node.cc
@@ -45,6 +45,26 @@ constexpr uint16_t BatteryVoltageToRawReading(float voltage) {
 }
 
 void setup() {
+  // check if nBOOT_SEL bit is set
+  if (FLASH->OPTR & FLASH_OPTR_nBOOT_SEL) {
+    // unlock flash/option
+    FLASH->KEYR = 0x45670123;
+    FLASH->KEYR = 0xCDEF89AB;
+    FLASH->OPTKEYR = 0x08192A3B;
+    FLASH->OPTKEYR = 0x4C5D6E7F;
+
+    while (FLASH->SR & FLASH_SR_BSY1)
+      ;
+
+    // clear nBOOT_SEL bit
+    FLASH->OPTR &= ~FLASH_OPTR_nBOOT_SEL;
+
+    // write
+    FLASH->CR |= FLASH_CR_OPTSTRT;
+    while (FLASH->SR & FLASH_SR_BSY1)
+      ;
+  }
+
   pinMode(kLedPin, OUTPUT);
   digitalWrite(kLedPin, HIGH);
 
