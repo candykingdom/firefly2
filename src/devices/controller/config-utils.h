f639decb477694166d933af91d553613e93f4b32
Ben Ryding <ryding.ben@gmail.com>
2026-07-15 15:07:08 -0600

    Clean up controller configuration state

--- src/devices/controller/config-utils.h
+++ src/devices/controller/config-utils.h
@@ -7,5 +7,6 @@ constexpr uint8_t ButtonRowToSlot(uint8_t row, bool right_side) {
 }
 
 constexpr uint8_t PreviousPaletteIndex(uint8_t current, uint8_t palette_count) {
-  return (current + palette_count - 1) % palette_count;
+  return palette_count == 0 ? current
+                            : (current + palette_count - 1) % palette_count;
 }
