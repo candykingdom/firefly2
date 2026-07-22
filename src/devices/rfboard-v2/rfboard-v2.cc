f29e8724eb529b9b5ec18c3b813acb70b5a0cbca
Adam DeMuri <adam.demuri@gmail.com>
2026-07-21 20:54:16 -0600

    rfboard-v2: add pin constants and set pin modes
    Change-Id: I1e1566cf956119c8d7fc42b5d8740782a6646732

--- src/devices/rfboard-v2/rfboard-v2.cc
+++ src/devices/rfboard-v2/rfboard-v2.cc
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+// The onboard addressable LED, which then feeds the external LED connector
+constexpr int kPinLedInternal = PA4; 
 constexpr int kPinRedLed = PB1;
+constexpr int kPinSw1 = PB6;
+constexpr int kPinSw2 = PB7;
+
+constexpr int kPin5vDivided = PA0;
+constexpr int kPinCc1 = PA1;
+constexpr int kPinCc2 = PB2;
+constexpr int kPinVibrationSensor = PA8;
 
 void setup() {
+  pinMode(kPinLedInternal, OUTPUT);
   pinMode(kPinRedLed, OUTPUT);
+  pinMode(kPinSw1, INPUT_PULLUP);
+  pinMode(kPinSw2, INPUT_PULLUP);
+
+  pinMode(kPin5vDivided, INPUT_ANALOG);
+  pinMode(kPinCc1, INPUT_ANALOG);
+  pinMode(kPinCc2, INPUT_ANALOG);
+  pinMode(kPinVibrationSensor, INPUT);
 }
 
 void loop() {
