8019ab2b2a1462143c9f4ea69c05106ff0a628d1
Adam DeMuri <adam.demuri@gmail.com>
2026-07-21 22:01:22 -0600

    Add network and led functionality to rfboard-v2
    Change-Id: I2a503610b96c6d7b2301b59575c128af63f353d9

--- src/devices/rfboard-v2/rfboard-v2.cc
+++ src/devices/rfboard-v2/rfboard-v2.cc
@@ -1,4 +1,16 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include <FlashStorage_STM32.h>
+
+#include <DeviceDescription.hpp>
+#include <Devices.hpp>
+#include <StripDescription.hpp>
+#include <vector>
+
+#include "../../arduino/FastLedManager.hpp"
+#include "../../arduino/RadioHeadRadio.hpp"
+#include "../../generic/NetworkManager.hpp"
+#include "../../generic/RadioStateMachine.hpp"
 
 // The onboard addressable LED, which then feeds the external LED connector
 constexpr int kPinLedInternal = PA4; 
@@ -11,6 +23,11 @@ constexpr int kPinCc1 = PA1;
 constexpr int kPinCc2 = PB2;
 constexpr int kPinVibrationSensor = PA8;
 
+RadioHeadRadio *radio = new RadioHeadRadio();
+NetworkManager network_manager(radio);
+RadioStateMachine state_machine(&network_manager);
+FastLedManager *led_manager;
+
 void setup() {
   pinMode(kPinLedInternal, OUTPUT);
   pinMode(kPinRedLed, OUTPUT);
@@ -21,8 +38,36 @@ void setup() {
   pinMode(kPinCc1, INPUT_ANALOG);
   pinMode(kPinCc2, INPUT_ANALOG);
   pinMode(kPinVibrationSensor, INPUT);
+
+  // TODO: support reading the device description from flash
+  led_manager = new FastLedManager(Devices::current, &state_machine);
+
+  if (!radio->Begin()) {
+    led_manager->FatalErrorAnimation();
+  }
+
+  // NOTE: can check if we watchdog rebooted by checking REG_PM_RCAUSE
+  // See https://github.com/gjt211/SAMD21-Reset-Cause
+  led_manager->PlayStartupAnimation();
+
+  // TODO: configure and enable the watchdog
 }
 
 void loop() {
   digitalWrite(kPinRedLed, (millis() / 100) % 2);
+  state_machine.Tick();
+  led_manager->RunEffect();
+
+  // Test code - remove after bringup
+  if (false) {
+    digitalWrite(kPinRedLed, digitalRead(kPinSw1));
+  }
+
+  if (false) {
+    digitalWrite(kPinRedLed, digitalRead(kPinSw2));
+  }
+
+  if (false) {
+    digitalWrite(kPinRedLed, digitalRead(kPinVibrationSensor));
+  }
 }
